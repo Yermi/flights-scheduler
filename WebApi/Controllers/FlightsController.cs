fcb1064353153537d2c18c9a57c88c85fc8633a6
Yirmiyahu Tessler <yermi72@mail.com>
2020-03-11 23:35:42 +0200

    add web api

---
+++ WebApi/Controllers/FlightsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class FlightsController : ApiController
+    {
+        public object Get()
+        {
+           return new { Fruit = "Strawberry", Topping = "Chocolate" };
+        }
+    }
+}
