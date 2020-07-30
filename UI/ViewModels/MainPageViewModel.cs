9ed207cc2e445f093fe95f0a0f612200af5aad84
Yirmiyahu Tessler <yermi72@mail.com>
2020-07-31 00:54:06 +0300

    add WPF project

---
+++ UI/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BE;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private readonly FlightsService _flightService;
+        public MainPageViewModel()
+        {
+            _flightService = new FlightsService();
+            Flights = new ObservableCollection<Flight>(_flightService.GetByDate(DateTime.Now));
+        }
+
+        public ObservableCollection<Flight> Flights { get; set; }
+    }
+}
